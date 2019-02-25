0c0c0e43428ceb8d604b937afbcefb71427c9365
jospic <jospic@gmail.com>
2019-02-25 10:12:49 +0100

    Mouse interaction

--- sun-sphere/Sprite.gd
+++ sun-sphere/Sprite.gd
@@ -1,13 +1,35 @@
 extends Sprite
 
-func _ready():
-    set_process(true)
-    pass
+var mouse 
+var screen
 
-func _process(delta):
-	var mouse = get_node("../").get_global_mouse_position();
-	var screen = get_viewport().get_visible_rect().size
-	mouse.x /= screen.x
-	mouse.y /= screen.y
+var mouse_in = false
+var dragging = false
 	
-	get_material().set_shader_param("mouse_position", mouse)
+func _ready():
+	screen = get_viewport().get_visible_rect().size	
+	get_material().set_shader_param("screen_size", screen)  # pass screen size on shader script
+	set_process(true)
+	
+func _process(delta):
+	if (mouse_in && Input.is_action_pressed("left_click")):
+		dragging = true
+				
+	if (dragging && Input.is_action_pressed("left_click")):
+		mouse = get_node("../").get_global_mouse_position();
+		mouse.x = mouse.x / screen.x * 100
+		mouse.y = mouse.y / screen.y * 100
+		
+		get_material().set_shader_param("mouse_position", mouse) # pass mouse position on shader script
+		
+	else:
+		dragging = false
+		
+
+func _on_Area2D_mouse_entered():
+	mouse_in = true
+
+
+func _on_Area2D_mouse_exited():
+	mouse_in = false
+
